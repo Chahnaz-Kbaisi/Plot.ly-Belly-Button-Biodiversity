3e18ff32790afd5357b456e0b55cc9edb572a7f5
Chahnaz-Kbaisi <chahnaz.k@comcast.net>
2020-12-15 13:39:19 -0500

    Updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // Step 1: Plotly
 // 1. Use the D3 library to read in `samples.json`.
-d3.json("../samples.json").then((data) => {
+d3.json("../data/sample.json").then((data) => {
     console.log(data);
     // Create the Traces
     // var trace1 = 
